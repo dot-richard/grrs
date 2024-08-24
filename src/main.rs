7a4fb286d402797e6d983ab580b1a69ab4660a4b
dot-richard <dot-richard@users.noreply.github.com>
2024-08-24 21:05:52 +0200

    first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use std::env;
+use std::path;
+use std::fs;
+use clap::Parser;
 
 // Recherche un pattern dans un fichier et affiche les lignes correspondantes.
 // Le pattern est la string que à chercher
 // Le path est le chemin du fichier à chercher
+#[derive(Parser)]
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf,
+    path: path::PathBuf,
 }
 
 fn main() {
-    let pattern = env::args().nth(1).expect("No pattern given.");
-    let path    = env::args().nth(2).expect("No path given");
-
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
-}
 
+    let content = fs::read_to_string(&args.path).expect("Could not read file.");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
