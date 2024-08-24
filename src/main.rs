11cd257407b5e671819e34910df972f053d1d51b
dot-richard <dot-richard@users.noreply.github.com>
2024-08-24 20:28:36 +0200

    afficher pattern et path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+// Recherche un pattern dans un fichier et affiche les lignes correspondantes.
+// Le pattern est la string que à chercher
+// Le path est le chemin du fichier à chercher
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
 }
+
+fn main() {
+    let pattern = env::args().nth(1).expect("No pattern given.");
+    let path    = env::args().nth(2).expect("No path given");
+
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
+
